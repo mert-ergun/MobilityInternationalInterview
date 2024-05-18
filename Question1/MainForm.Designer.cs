40b2df2d57e8c46956b67f0abe9b9fc44190d4c7
Mert ERGÜN <mertergun17@hotmail.com>
2024-05-18 23:53:08 +0300

    Question 1 done

--- Question1/MainForm.Designer.cs
+++ Question1/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Question1
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.btnFindPrimes1 = new System.Windows.Forms.Button();
+            this.btnFindPrimes2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Location = new System.Drawing.Point(12, 12);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(153, 20);
+            this.txtNumber1.TabIndex = 0;
+            this.txtNumber1.Text = "Write the Value 1";
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNumber2.Location = new System.Drawing.Point(240, 12);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(153, 20);
+            this.txtNumber2.TabIndex = 1;
+            this.txtNumber2.Text = "Write the Value 2";
+            // 
+            // btnFindPrimes1
+            // 
+            this.btnFindPrimes1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnFindPrimes1.Location = new System.Drawing.Point(12, 345);
+            this.btnFindPrimes1.Name = "btnFindPrimes1";
+            this.btnFindPrimes1.Size = new System.Drawing.Size(153, 42);
+            this.btnFindPrimes1.TabIndex = 2;
+            this.btnFindPrimes1.Text = "Find Primes 1";
+            this.btnFindPrimes1.UseVisualStyleBackColor = true;
+            this.btnFindPrimes1.Click += new System.EventHandler(this.btnFindPrimes1_Click);
+            // 
+            // btnFindPrimes2
+            // 
+            this.btnFindPrimes2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFindPrimes2.Location = new System.Drawing.Point(240, 345);
+            this.btnFindPrimes2.Name = "btnFindPrimes2";
+            this.btnFindPrimes2.Size = new System.Drawing.Size(153, 42);
+            this.btnFindPrimes2.TabIndex = 3;
+            this.btnFindPrimes2.Text = "Find Primes 2";
+            this.btnFindPrimes2.UseVisualStyleBackColor = true;
+            this.btnFindPrimes2.Click += new System.EventHandler(this.btnFindPrimes2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 56);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(153, 264);
+            this.listBox1.TabIndex = 4;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(240, 56);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(153, 264);
+            this.listBox2.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(405, 427);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnFindPrimes2);
+            this.Controls.Add(this.btnFindPrimes1);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtNumber1);
+            this.Name = "MainForm";
+            this.Text = "Find Primes Up to N";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.Button btnFindPrimes1;
+        private System.Windows.Forms.Button btnFindPrimes2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
     }
 }
 
--- Question1/MainForm.cs
+++ Question1/MainForm.cs
@@ -1,20 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Question1
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
+
+        private void btnFindPrimes1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtNumber1.Text, out int number))  // Check if the input is a valid number.
+            {
+                Thread thread = new Thread(() => FindPrimes(number, listBox1));  // Create a new thread to find prime numbers.
+                thread.Start();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number.");
+            }
+        }
+
+        private void btnFindPrimes2_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtNumber2.Text, out int number)) 
+            {
+                Thread thread = new Thread(() => FindPrimes(number, listBox2)); 
+                thread.Start();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number.");
+            }
+        }
+
+        /// <summary>
+        /// Find prime numbers up to a given number and display them in a list box.
+        /// Call SieveOfEratosthenes method to find prime numbers.
+        /// </summary>
+        /// <param name="number"> The number up to which to find prime numbers.</param>
+        /// <param name="listBox"> The list box in which to display the prime numbers.</param>
+        private void FindPrimes(int number, ListBox listBox)
+        {
+            List<int> primes = SieveOfEratosthenes(number);
+            listBox.Invoke(new Action(() =>
+            {
+                listBox.Items.Clear();
+                foreach (int prime in primes)
+                {
+                    listBox.Items.Add(prime);
+                }
+            }));
+        }
+
+        /// <summary>
+        /// Find all prime numbers up to a given number using the Sieve of Eratosthenes algorithm.
+        /// </summary>
+        /// <param name="number">The number up to which to find prime numbers.</param></param>
+        /// <returns> A list of prime numbers up to the given number.</returns>
+        private List<int> SieveOfEratosthenes(int number)
+        {
+            bool[] isPrime = new bool[number + 1];
+            for (int i = 2; i <= number; i++) isPrime[i] = true;
+
+            for (int p = 2; p * p <= number; p++)
+            {
+                if (isPrime[p])
+                {
+                    for (int i = p * p; i <= number; i += p)
+                    {
+                        isPrime[i] = false;
+                    }
+                }
+            }
+
+            List<int> primes = new List<int>();
+            for (int i = 2; i <= number; i++)
+            {
+                if (isPrime[i]) primes.Add(i);
+            }
+
+            return primes;
+        }
+
     }
 }
