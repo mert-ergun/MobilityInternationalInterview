da333271c0a30f48c8ff5e203e4981c9c7045d47
Mert ERGÜN <mertergun17@hotmail.com>
2024-05-19 22:14:25 +0300

    Added migrations and created DB

---
+++ Question2/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Question2.Models;
+
+namespace Question2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
--- Question2/Program.cs
+++ Question2/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Question2.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Configure the database context
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
